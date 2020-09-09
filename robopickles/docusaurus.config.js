6dbd6440675eed9d1ecf1cd6f2614613c2c423d8
Kirill Pinchuk <cybergrind@gmail.com>
2020-09-09 22:12:23 +0300

    disable side links

--- robopickles/docusaurus.config.js
+++ robopickles/docusaurus.config.js
@@ -15,12 +15,12 @@ module.exports = {
         src: 'img/logo.svg',
       },
       items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
+        //        {
+        //          to: 'docs/',
+        //          activeBasePath: 'docs',
+        //          label: 'Docs',
+        //          position: 'left',
+        //        },
         { to: 'blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/robopickles/',
@@ -32,52 +32,52 @@ module.exports = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href:
-                'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+        //        {
+        //          title: 'Docs',
+        //          items: [
+        //            {
+        //              label: 'Style Guide',
+        //              to: 'docs/',
+        //            },
+        //            {
+        //              label: 'Second Doc',
+        //              to: 'docs/doc2/',
+        //            },
+        //          ],
+        //        },
+        //        {
+        //          title: 'Community',
+        //          items: [
+        //            {
+        //              label: 'Stack Overflow',
+        //              href:
+        //                'https://stackoverflow.com/questions/tagged/docusaurus',
+        //            },
+        //            {
+        //              label: 'Discord',
+        //              href: 'https://discordapp.com/invite/docusaurus',
+        //            },
+        //            {
+        //              label: 'Twitter',
+        //              href: 'https://twitter.com/docusaurus',
+        //            },
+        //          ],
+        //        },
+        //        {
+        //          title: 'More',
+        //          items: [
+        //            {
+        //              label: 'Blog',
+        //              to: 'blog',
+        //            },
+        //            {
+        //              label: 'GitHub',
+        //              href: 'https://github.com/facebook/docusaurus',
+        //            },
+        //          ],
+        //        },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} RoboPickles.`,
     },
     googleAnalytics: {
       trackingID: 'UA-176711841-1',
